6254e61c54aabcf64b084085efabdfe2918b769a
Maria <pavla0806@gmail.com>
2023-06-13 22:30:24 +0300

    a program that outputs a random number from the segment [10, 99] and shows the largest digit of the number

---
+++ 2_5/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// Напишите программу, которая принимает на 
+// вход два числа и проверяет, является ли одно 
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+int number_1 = int.Parse(Console.ReadLine()!);
+int number_2 = int.Parse(Console.ReadLine()!);
+if (number_1 * number_1 == number_2 || number_2 * number_2 == number_1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
