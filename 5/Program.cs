90a376ff0d64632e1c977f0b3a5cd60ff6e2aecc
Maria <pavla0806@gmail.com>
2023-06-13 17:29:30 +0300

    a program that accepts a three-digit number as input and shows the last digit of this number at the output

---
+++ 5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+//  number[0]
+
+
+
+int number = int.Parse(Console.ReadLine()!);
+if (100 < number && number < 1000)
+{
+    int result = number % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число");
+}
