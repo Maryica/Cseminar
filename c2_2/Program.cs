d4df35a32b9833c83474cb9df5e8d76feca9bc17
Maria <pavla0806@gmail.com>
2023-06-13 22:23:14 +0300

    a program that outputs a random three-digit number and deletes the second digit of that number

---
+++ c2_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.Clear();
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int firstValue = number / 100;
+int thirdValue = number % 10;
+int newNumber = firstValue * 10 + thirdValue;
+Console.WriteLine(newNumber);
+
+
+
+
