707d64f4fdd4ed06fa2e4736356a309ac51602d9
Maria <pavla0806@gmail.com>
2023-06-13 22:26:19 +0300

    a program that will take on input two numbers and output whether the second number is a multiple of the first. If the number 2 is not a multiple of the number 1, then the program outputs the remainder of the division

---
+++ 2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая будет принимать на 
+// вход два числа и выводить, является ли второе число 
+// кратным первому. Если число 2 не кратно числу 1, то 
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+int number_1 = int.Parse(Console.ReadLine()!);
+int number_2 = int.Parse(Console.ReadLine()!);
+int reminder = number_1 % number_2;
+
+if (number_1 % number_2 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратно, остаток " + reminder);
+}
